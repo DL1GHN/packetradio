91984b7a2581e93f174f2cae2fc4e6ced6744235
DL1GHN <65889831+DL1GHN@users.noreply.github.com>
2021-01-06 13:36:31 +0100

    Update m_filter.h
    Hallo,
    anbei die Änderungen von Daniel DL7NDR. Diese Version ist bei DB0LHR-8 und DB0RBS-8 getestet.
    
    73 de Pat, DL1GHN
    Sysop DB0LHR

--- m_filter/m_filter.h
+++ m_filter/m_filter.h
@@ -1,24 +1,24 @@
-/***************************************************************/
-/* m_filter.prg zum Funkruf-Server                             */
-/* Stand 15.10.2006  DH6BB                                     */
-/***************************************************************/
+// ***************************************************************
+// m_filter.prg zum Funkruf-Server
+// Stand 15.10.2006  DH6BB
+// Stand 2020-07-14 DL7NDR
+// ***************************************************************
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <libgen.h>
 
-//#define DEBUG
-//#define BCM                   // <== Fuer die BayBox die "//" entfernen!!
-#define Boxcall    "DB0WHV"	// <== A E N D E R N !!!  (ohne SSID)
-#define Mailinfo   "/usr/local/funkruf/lib/mailinfo"
+// #define DEBUG
+#define BCM                   					// <== Fuer die BayBox die "//" vor "#define" entfernen!!
+#define Boxcall    "DB0XXX"				// <== A E N D E R N !!!  (ohne SSID)
+#define Mailinfo   "/home/bcm/mailinfo"				// urspruenglich "/usr/local/funkruf/lib/mailinfo"
 
-#DAPNET
-#define DAPNET_URL			http://dapnet.afu.rwth-aachen.de/api/calls
-#define DAPNET_NAME			bcm
-#define DAPNET_PASSWORD		
+#define DAPNET_URL		"http://44.149.166.27:8080/calls"	// oder dapnet.db0sda.ampr.org:8080/calls oder via Internet: http://www.hampager.de:8080/calls
+#define DAPNET_NAME		"dl7ndr"			// DAPNET-Accounts fÃ¼r Mailboxen werden nicht angelegt. Es mÃ¼ssen Personenaccounts verwendet werden.
+#define DAPNET_PASSWORD	"passwortmeinesdapnetaccounts"
 
-#define KEPLER
+// #define KEPLER
 #ifdef KEPLER
  #define KeplerCall   "CX2SA"
  #define KeplerRubrik "KEPS"
@@ -26,7 +26,7 @@
  #define Keplerfile   "/usr/local/funkruf/lib/kepler.txt"
 #endif
 
-//********* Ab hier nichts mehr aendern !!! *************
+// ********* Ab hier nichts mehr aendern !!! *************
 #define Version    0.26
 
 #ifndef BCM
